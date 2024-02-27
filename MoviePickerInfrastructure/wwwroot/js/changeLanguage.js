07fed612894dc951c8f378ef28a88c487f3a0dfe
AndriiStaryk <andrystarik@gmail.com>
2024-02-27 08:45:22 +0200

    added little design

---
+++ MoviePickerInfrastructure/wwwroot/js/changeLanguage.js
@@ -0,0 +1,17 @@
+﻿function changeLanguage(languageCode) {
+    var currentUrl = window.location.href;
+    var newUrl;
+
+    // Check if the current URL already contains a query string
+    if (currentUrl.indexOf('?') !== -1) {
+        // URL contains query string
+        newUrl = currentUrl.replace(/(\?|&)ui-culture=[^&]*|$/, '$1ui-culture=' + languageCode);
+    } else {
+        // URL doesn't contain query string
+        newUrl = currentUrl + '?ui-culture=' + languageCode;
+    }
+
+    // Redirect to the new URL
+    window.location.href = newUrl;
+}
+
