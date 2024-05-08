2fc30d54198e867a04c3aeda41b56a1944f3018f
AndriiStaryk <andrystarik@gmail.com>
2024-05-08 19:56:44 +0300

    added requests page

---
+++ MoviePickerInfrastructure/Controllers/DB_LabController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviePickerInfrastructure.Controllers;
+
+public class DB_LabController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Request1()
+    {
+        return View();
+    }
+}
